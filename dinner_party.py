7e83662b4f05b261728adcdba4eebe793b98cdeb
Jordan Le <jor25@pdx.edu>
2019-10-05 18:15:34 -0700

    Basic set up for file.

---
+++ dinner_party.py
@@ -0,0 +1,37 @@
+# Artificial Intelligence: Dinner Party
+''' Given a set of n people (n is even), they will be sat on
+    two sides of a long table.
+
+      o   o   o      o
+   +-------------   ----+
+   |             ...    | 
+   +-------------   ----+ 
+      o   o   o      o
+
+    Half are "hosts" half are "guests".
+'''
+# Date: 10-5-19
+#
+
+def main():
+    pass
+
+
+# Return the role of the person - host or guest?
+def role(person):
+    pass
+
+
+# How much the 1st person likes the 2nd person - May be negative.
+def preferance(p1, p2):
+    pass
+
+
+# Determine Scoring
+def scoring(data):
+    pass
+
+
+if __name__== "__main__" :
+    main()
+
