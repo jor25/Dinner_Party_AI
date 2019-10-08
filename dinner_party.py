29b48a38e84dde6aca10365480a843c7a590a0f4
Jordan Le <jor25@pdx.edu>
2019-10-08 12:15:09 -0700

    File read in using numpy operational. Update for command line args later.

--- dinner_party.py
+++ dinner_party.py
@@ -11,25 +11,35 @@
     Half are "hosts" half are "guests".
 '''
 # Date: 10-5-19
-#
+# Run with "python3 dinner_party.py"
+# Installs "pip3 install <package>"
 
-# Read the text file data into 2d array
-def read_data():
-	pass
+import numpy as np
+
+# Read the text file data into 2d array. Give back 2d array and num.
+def read_data(data_file="data_insts/hw1-inst1.txt"):
+	
+	# Numpy read in my data - separate by space, skip row 1, all ints.	
+	data = np.loadtxt(data_file, delimiter=" ", skiprows=1, dtype=int)
+	num_p = len(data)
+	print(data)
+	print(num_p)
+	
+	return data, num_p
 
 
 def main():
-    pass
+	pass
 
 
 # Return the role of the person - host or guest?
 def role(person):
-    pass
+	pass
 
 
 # How much 1st person likes 2nd person - May be negative.
 def preferance(p1, p2):
-    pass
+	pass
 
 
 # Determine Scoring
@@ -44,9 +54,10 @@ def preferance(p1, p2):
 		or opposite pair of people p1, p2.
 '''
 def scoring(data):
-    pass
+	pass
 
 
 if __name__== "__main__" :
-    main()
+	main()
+	read_data()
 
