6045804a4ad80a5a7414a50728531bc9533d8874
Jordan Le <jor25@pdx.edu>
2019-10-23 11:38:35 -0700

    Commenting and formatting of local search.

--- agent_file.py
+++ agent_file.py
@@ -7,10 +7,9 @@ import dinner_party as dp
 # Referance:
 # Index Swap: https://stackoverflow.com/questions/22847410/swap-two-values-in-a-numpy-array
 
-
 def swap(cord1, cord2, s_table):
-    ''' Given a semi optimized table, perform a switch based on given coordinates.
-        If the score of the switch is suboptimal, switch it back.
+    ''' Given two coordinates for elements in the 2d table and a semi optimized table,
+        perform a switch based on the given coordinates.
     '''
     r1 = cord1[0][0]    # Get row index of cord1
     c1 = cord1[1][0]    # Get col index of cord1
@@ -20,39 +19,34 @@ def swap(cord1, cord2, s_table):
 
     # Do fast elements swap
     s_table[r1][c1], s_table[r2][c2] = s_table[r2][c2], s_table[r1][c1]
-    #print(s_table)
 
 
 def local_search(s_table, pref_summed, num_p):
+    ''' Given the seated table, the transposed summed preferance matrix, and the number of people,
+        calculate the optimized seated table. Then go through each person and switch them with
+        another person to see if they have improved score. If they don't, swap them back. Next,
+        move on to the next person and repeat the process for each set of people. Finally,
+        give back the most optimal solution for the given table.
     '''
-    '''
-    sub_highest = dp.score_fast(s_table, pref_summed, num_p)   # Faster table scoring
-    sub_fin_table = s_table
-    #print("Sub High Score: {}\n{}\n".format(sub_highest, sub_fin_table))
+    sub_highest = dp.score_fast(s_table, pref_summed, num_p)    # Faster table scoring
+    sub_fin_table = s_table                                     # Set the sub final table
 
     for i in range(num_p):
-        # Get person's coord's
-        cord_i = np.where(s_table == i)
-        #print(cord_i)
-        #print(cord_i[0][0])
-        for j in range(num_p):
-            if i == j:
-                pass
-            else:
-                #print("i:", i, "\tj:", j)
-                cord_j = np.where(s_table == j)
-                swap(cord_i, cord_j, s_table)
-                temp_score = dp.score_fast(s_table, pref_summed, num_p)
+        cord_i = np.where(s_table == i) # Get person i's coordinates
 
-                if temp_score > sub_highest:
-                    #print("Sub: {}\tTemp: {}".format(sub_highest, temp_score))
-                    sub_highest = temp_score
-                    sub_fin_table = s_table
-                    # Either break or look for optimal - break first
-                    break
-                else:
-                    # Swap it back - didn't improve score
-                    #print("SWAP BACK")
-                    swap(cord_j, cord_i, s_table)
+        for j in range(num_p):          # Loop through all other people
+            if i == j:                  # Don't swap the element with itself
+                pass                    # Nothing to do this time
+            else:
+                cord_j = np.where(s_table == j)                             # Give me the coordinates of j
+                swap(cord_i, cord_j, s_table)                               # Swap where i and j are
+                temp_score = dp.score_fast(s_table, pref_summed, num_p)     # Calculate score fast
+
+                if temp_score > sub_highest:        # If higher score, do updates
+                    sub_highest = temp_score        # Update the sub highest score
+                    sub_fin_table = s_table         # Update sub final table
+                    break                           # Either break or look for optimal...
+                else:           
+                    swap(cord_j, cord_i, s_table)   # Swap it back, no improvement
 
     return sub_highest, sub_fin_table   # Give back the best from sub optimizations
